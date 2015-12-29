2c7b33a8f37416ca445b6a928a31144ca4aeb9c5
Mario Pena <mariop08@csu.fullerton.edu>
2015-12-28 23:25:43 -0800

    Current App uploaded

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leagueApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the leagueApp
+ */
+angular.module('leagueApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    //change the active navigation menu
+    angular.element('.nav li.active').removeClass('active');
+    angular.element('.nav li').eq(1).addClass('active');
+
+  });
+
+
---
+++ app/scripts/controllers/search.js
@@ -0,0 +1,105 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leagueApp.controller:SearchBarCtrl
+ * @description
+ * # SearchBarCtrl
+ * Controller of the leagueApp
+ */
+angular.module('leagueApp')
+  .controller('SearchBarCtrl', ['$scope', '$http' , 'summonerFactory' , function ($scope, $http, summonerFactory) {
+
+	  $scope.regions = [
+					  'NA',
+					  'KR',
+					  'LAN',
+					  'EUW',
+					  'RU',
+					  'TR',
+					  'BR',
+					  'OCE',
+					  'LAS'
+					  ];
+
+	  $scope.selectedRegion = 'NA';
+
+
+
+    $scope.dropDownSelectedItem = function(region){
+      $scope.selectedRegion = region;
+
+      console.log($scope.selectedRegion);
+    };
+
+    //$scope.submitSummoner = function (summoner) {
+
+    //  summoner.region = $scope.selectedRegion;
+
+      //$scope.summonerID = SearchService.idLookup(summoner);
+
+      //$http.get('https://'+summoner.region+'.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+summoner.name
+      //  +'?api_key=29993728-af63-4ecd-84d8-fdd068d9d11a')
+      //  .success(function(res) {
+      //    $scope.info = res[summoner.name];
+      //
+      //    console.log($scope.info);
+      //  });
+
+
+    //}
+
+    $scope.submitSummoner = function getSummonerID(summoner) {
+
+      summoner.region = $scope.selectedRegion;
+      console.log(summoner.region);
+
+      summonerFactory.getSummonerID(summoner)
+        .success(function (res){
+          summoner.id = res[summoner.name].id;
+          console.log(summoner);
+
+          //need a separate username variable without the two way binding
+          summoner.username = res[summoner.name].name;
+          summoner.profileIconSrc = "http://ddragon.leagueoflegends.com/cdn/5.24.2/img/profileicon/"+res[summoner.name].profileIconId+".png";
+          summoner.level = res[summoner.name].summonerLevel;
+
+          $scope.getSummonerMasteries(summoner);
+          $scope.getSummonerRunes(summoner);
+
+          //for controlling ng-show directive in main.html
+          summoner.show = true;
+          summoner.error = false;
+        })
+        .error(function (error){
+          console.log("Unable to get Summoner ID data: " + error.message);
+
+          //for controlling ng-show directive in main.html
+          summoner.error = true;
+          summoner.show = false;
+        });
+    };
+
+    $scope.getSummonerMasteries = function (summoner) {
+      summonerFactory.getSummonerMasteries(summoner)
+        .success( function (res) {
+          summoner.masteries = res[summoner.id].pages;
+          console.log(summoner.masteries);
+        })
+        .error(function (error) {
+          console.log("Unable to get Summoner Masteries: " + error.message);
+        });
+    };
+
+    $scope.getSummonerRunes = function (summoner) {
+      summonerFactory.getSummonerRunes(summoner)
+        .success( function (res) {
+          summoner.runes = res[summoner.id].pages;
+          console.log(summoner.runes);
+        })
+        .error(function (error) {
+          console.log("Unable to get Summoner Runes: " + error.message);
+        });
+    };
+
+  }]);
