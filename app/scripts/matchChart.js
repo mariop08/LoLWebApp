4715dec9279867706df88c3671f160306a5d90d8
Ah Som Choi <magodpdy@gmail.com>
2016-02-14 13:42:31 -0800

    haha

---
+++ app/scripts/matchChart.js
@@ -0,0 +1,67 @@
+var chart = dc.pieChart("#pieChart1");
+//static data array for now
+var totalDamageDealtArray = [
+      {
+        "championId" : 78,
+        "totalDamageDealt" : 11111
+      },
+      {
+        "championId" : 71,
+        "totalDamageDealt" : 33333
+      },
+      {
+        "championId" : 72,
+        "totalDamageDealt" : 80000
+      },
+      {
+        "championId" : 73,
+        "totalDamageDealt" : 22345
+      },
+      {
+        "championId" : 74,
+        "totalDamageDealt" : 33421
+      },
+      {
+        "championId" : 75,
+        "totalDamageDealt" : 11245
+      },
+      {
+        "championId" : 76,
+        "totalDamageDealt" : 4794
+      },
+      {
+        "championId" : 77,
+        "totalDamageDealt" : 23545
+      },
+      {
+        "championId" : 79,
+        "totalDamageDealt" : 60075
+      },
+      {
+        "championId" : 70,
+        "totalDamageDealt" : 37545
+      }
+    ];
+
+var ndx = crossfilter(totalDamageDealtArray);
+
+//Dimension and Group for damage dealt by champion Pie Chart
+var damageDim = ndx.dimension(function(d){return "champion "+d.championId+"'s total damage dealt:"+d.totalDamageDealt;});
+var damageGroup = damageDim.group().reduceSum(function(d) {return d.totalDamageDealt;});
+
+
+
+
+chart.width(450)
+         .height(400)
+         .transitionDuration(1000)
+         .dimension(damageDim)
+         .group(damageGroup)
+         .radius(180)
+         .innerRadius(90)
+         .minAngleForLabel(0)
+         .renderLabel(false)
+         .title(function(d){ console.log(d); return d.key; });
+
+
+chart.render();
