38078d5f43e689fa7a22e0608a40e8ea3dbe2263
Ah Som Choi <magodpdy@gmail.com>
2016-02-15 22:35:23 -0800

    wrapped matchChart.js in a function and called it in match.js

--- app/scripts/controllers/match.js
+++ app/scripts/controllers/match.js
@@ -9,7 +9,7 @@
  */
 leagueApp
   .controller('MatchCtrl', ['$scope' , 'summonerFactory' , function ($scope, summonerFactory) {
-    $scope.getMatchList = function(summoner) {
+    $scope.getMatch = function(summoner) {
 
       match.region = $scope.selectedRegion;
 
@@ -25,8 +25,8 @@ leagueApp
           // summoner.show = false;
         });
     };
-    var chart = dc.pieChart("#pieChart1");
-    //static data array for now
+    
+    //data array
     var totalDamageDealtArray = [
           {
             "championId" : 78,
@@ -70,33 +70,6 @@ leagueApp
           }
         ];
 
-    var ndx = crossfilter(totalDamageDealtArray);
-
-    //Dimension and Group for damage dealt by champion Pie Chart
-    var damageDim = ndx.dimension(function(d){return "champion "+d.championId+"'s total damage dealt:"+d.totalDamageDealt;});
-    var damageGroup = damageDim.group().reduceSum(function(d) {return d.totalDamageDealt;});
-
-    //Custom Styling for Tool Tip for Pie Chart #1
-    var pieTip1 = d3.tip()
-      .attr('class','d3-tip')
-      .offset([-10,0])
-      .html(function (d) { return "<span style='color: #3369ff'>" +  d.data.key + "</span> : "  + d.data.value; });
-
-    chart.width(450)
-             .height(400)
-             .transitionDuration(1000)
-             .dimension(damageDim)
-             .group(damageGroup)
-             .radius(180)
-             .innerRadius(0)
-             .minAngleForLabel(0)
-             .renderLabel(false)
-             .title(function(d){return d.key;});
-
-    dc.renderAll();
-
-    d3.selectAll(".pie-slice").call(pieTip1);
-                  d3.selectAll(".pie-slice").on('mouseover', pieTip1.show)
-                      .on('mouseout', pieTip1.hide);
+        createPieChart("pieChart1", totalDamageDealtArray);
 
   }]);
